cffe62a94d4a44a5911a965d338a0023a5a7f03d
Amna Lodhi <amna78058@gmail.com>
2025-06-01 00:18:44 +0500

    HashingSum.java

--- hashingSum.java
+++ hashingSum.java
@@ -1,23 +1,27 @@
 import java.util.*;
 
-class HashingSum {
+class Sum {
     public static void main(String[] args) {
-        int[] arr = { 1, -1, 0, 2, -1, -4 };
-        Arrays.sort(arr);
+        int[] arr = {1, -1, 0, 2, -1, -4};
         int len = arr.length;
-		// set of lists of integers
+
+        // Set of lists to store unique triplets
         Set<List<Integer>> uniqueTriplets = new HashSet<>();
 
+        // Iterate through the array
         for (int i = 0; i < len; i++) { // a
+            int target = arr[i];
+            Set<Integer> s = new HashSet<>();
+
             for (int j = i + 1; j < len; j++) { // b
-                for (int k = j + 1; k < len; k++) { // c
-                    if (arr[i] + arr[j] + arr[k] == 0) {
-					    //stores array values as list
-                        List<Integer> triplet = Arrays.asList(arr[i], arr[j], arr[k]);
-                        Collections.sort(triplet);  ///sorting list would help recognize duplicates
-                        uniqueTriplets.add(triplet); 
-                    }
+                int toFind = -(target + arr[j]);
+                if (s.contains(toFind)) {
+                    // Store the triplet in a list
+                    List<Integer> triplet = Arrays.asList(arr[i], arr[j], toFind);
+                    Collections.sort(triplet); // Sorting helps avoid duplicates
+                    uniqueTriplets.add(triplet);
                 }
+                s.add(arr[j]);
             }
         }
 
